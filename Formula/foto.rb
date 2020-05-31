7bef9949927f719e7061b724dc847121cf333963
capaldo <alex@capaldo.io>
2020-05-31 00:38:28 -0400

    fix test declaration

--- Formula/foto.rb
+++ Formula/foto.rb
@@ -23,6 +23,6 @@ class Foto < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/foto"
   end
 end
