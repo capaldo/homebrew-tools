6ef8b9fb24658ba892f28fe6a1a55ca18bdecbe4
capaldo <alex@capaldo.io>
2020-05-31 00:27:10 -0400

    initial commit

---
+++ Formula/foto.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Foto < Formula
+  desc "A Swift command-line tool to copy and paste images from your clipboard"
+  homepage "https://github.com/capaldo/foto"
+  url "https://github.com/capaldo/foto.git",
+    :tag => "0.1"
+  version "0.1"
+  sha256 "9611a70c5f3ce5d6b1b992a9e5b056ce377269f19418432a4ce287fb09675269"
+
+  depends_on :xcode => ["11.5", :build]
+
+  def install
+    system "swift", "build",
+      "--configuration", "release",
+      "--disable-sandbox"
+    bin.install '.build/release/your-project'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test foto`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# Capaldo Tools
+
+## How do I install these formulae?
+`brew install capaldo/tools/<formula>`
+
+Or `brew tap capaldo/tools` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
