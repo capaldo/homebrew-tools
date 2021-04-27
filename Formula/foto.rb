024720a1c25812470c329a1fd40f8e096d88651f
capaldo <alex@capaldo.io>
2021-04-27 19:19:34 -0400

    upgrade foto to 0.2

--- Formula/foto.rb
+++ Formula/foto.rb
@@ -2,7 +2,7 @@ class Foto < Formula
   desc "A Swift command-line tool to copy and paste images from your clipboard"
   homepage "https://github.com/capaldo/foto"
   url "https://github.com/capaldo/foto.git"
-  version "0.1.3"
+  version "0.2"
   sha256 "c9e9d526cb73d5752bc5b140fffd06893c9832d57fc5f3a260972f3ea2929431"
   head "https://github.com/capaldo/foto.git"
 
